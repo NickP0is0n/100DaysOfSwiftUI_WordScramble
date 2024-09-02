2609fd6808c5886ca79fe805322affd8e67a89a0
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-02 17:01:14 +0300

    Generated-only list

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,25 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Henry", "Nick", "Dawn"]
     var body: some View {
-        List {
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-            
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-            
-            Section("Section 3") {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
+        List(people, id: \.self) {
+            Text($0)
         }
-        .listStyle(.grouped)
     }
 }
 
