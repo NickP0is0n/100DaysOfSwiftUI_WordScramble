6948f2a5da9ca0398fcbfd2a66aa41e50aec5fff
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-02 16:43:19 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Mykola Chaikovskyi on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
