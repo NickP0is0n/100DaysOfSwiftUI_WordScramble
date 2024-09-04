97889d9b43b68dcf8a1d17f5b6b7ce67938ec248
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-04 12:55:12 +0300

    Completed challenges

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,11 +12,28 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    var score: Int {
+        var score = 0
+        for word in usedWords {
+            score += word.count * 100
+        }
+        return score
+    }
 
     var body: some View {
         VStack {
             NavigationStack {
                 List {
+                    Section {
+                        Text("Score: \(score)")
+                            .font(.headline.bold())
+                    }
+                    
                     Section {
                         TextField("Enter your word", text: $newWord)
                             .textInputAutocapitalization(.never)
@@ -34,6 +51,13 @@ struct ContentView: View {
                 .navigationTitle(rootWord)
                 .onSubmit(addNewWord)
                 .onAppear(perform: startGame)
+                .alert(errorTitle, isPresented: $showingError) {
+                } message: {
+                     Text(errorMessage)
+                }
+                .toolbar {
+                    Button("New game", action: startGame)
+                }
             }
         }
     }
@@ -43,6 +67,31 @@ struct ContentView: View {
         
         guard answer.count > 0 else {
             return
+        }
+        
+        guard isEnoughSize(word: answer) else {
+            wordError(title: "Word is too short", message: "You need to use at least three letters, think again!")
+            return
+        }
+        
+        guard isNotRootWord(word: answer) else {
+            wordError(title: "Word is the same as original", message: "You can not just go ahead with same word displayed on the screen!")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know it!")
+            return 
         }
         
         withAnimation {
@@ -55,6 +104,7 @@ struct ContentView: View {
     func startGame() {
         if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsUrl) {
+                usedWords.removeAll()
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "bookworm"
                 return
@@ -63,7 +113,45 @@ struct ContentView: View {
         
         fatalError("Could not load start.txt from the bundle.")
     }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
 
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func isEnoughSize(word: String) -> Bool {
+        word.count > 2
+    }
+    
+    func isNotRootWord(word: String) -> Bool {
+        word != rootWord
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
